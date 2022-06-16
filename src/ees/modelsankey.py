5ce2de1fb1fdaa83db3a7281490c51158a622d57
arthurpmrs <arthurpmrs@gmail.com>
2022-06-16 12:22:38 -0300

    Refactoring of sankey generation script.

--- src/ees/modelsankey.py
+++ src/ees/modelsankey.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import math
-
+import plotly.io as pio
 from numpy import i0
 sys.path.append(os.path.join(os.getcwd(), 'src'))
 from utilities import get_base_folder
@@ -11,14 +11,15 @@ from matplotlib.sankey import Sankey
 import plotly.graph_objects as go
 import random
 
+pio.kaleido.scope.mathjax = None
+
 
 class ModelSankey:
 
-    def __init__(self, EES_model: str, run_id: str, display_title: str):
+    def __init__(self, EES_model: str, run_id: str):
         self.base_path = get_base_folder(EES_model)
         self.run_id = run_id
         self.paths = self.set_paths()
-        self.display_title = display_title
 
     def set_paths(self):
         paths = {
@@ -31,7 +32,7 @@ class ModelSankey:
 
         return paths
 
-    def load_exergy_data(self):
+    def load_data(self) -> list:
         filename_arrays = os.path.join(self.paths.get("data"), "clean_arrays.csv")
         df_arrays = pd.read_csv(filename_arrays, delimiter=";", decimal=",")
         exergy = df_arrays["ex"]
@@ -40,9 +41,6 @@ class ModelSankey:
         filename_outputs = os.path.join(self.paths.get("data"), "outputs.xlsx")
         outputs = pd.read_excel(filename_outputs)
         outputs = outputs.set_index("Unnamed: 0")
-
-        labels = ["Combustível", "Brayton", "SRA", "HDH", "Produtos", "Perdas", "Exaustão Brayton",
-                  "Exaustão SRA", "Exaustão HDH"]
 
         data = [
             ["Combustível", "Brayton", exergy[9]],
@@ -58,6 +56,8 @@ class ModelSankey:
             ["HDH", "Produtos", exergy[38]],
             ["HDH", "Perdas", outputs.loc["Exd_hdh", 0]]
         ]
+        labels = ["Combustível", "Brayton", "SRA", "HDH", "Produtos", "Perdas", "Exaustão Brayton",
+                  "Exaustão SRA", "Exaustão HDH"]
         for i, label in enumerate(labels):
             for j, t in enumerate(data):
                 for k, item in enumerate(t):
@@ -65,28 +65,44 @@ class ModelSankey:
                         data[j][k] = i
         return data
 
-    def get_titles(self):
-        pass
-
-    def generate(self):
-        data = self.load_exergy_data()
+    def prepare_componentes(self, lang: str = "pt") -> dict:
+        data = self.load_data()
 
         source = [item[0] for item in data]
         target = [item[1] for item in data]
         value = [item[2] for item in data]
-        labels = ["Combustível", "Brayton", "SRA", "HDH", "Produtos", "Perdas", "Exaustão Brayton",
-                  "Exaustão SRA", "Exaustão"]
 
-        node_colors = ["rgba(255, 215, 0, 1)", "rgba(255,0,255, 1)", "rgba(0,255,0, 1)", "rgba(0, 0, 255, 1)",
-                       "rgba(255, 215, 0, 1)", "rgba(255,0,0, 1)", "rgba(255,0,255, 1)", "rgba(0,255,0, 1)", "rgba(0, 0, 255, 1)"]
-        link_colors = [color.replace("1)", "0.4)") for color in node_colors]
+        if lang.lower() == "pt":
+            labels = ["Combustível", "Brayton", "SRA", "HDH", "Produtos", "Perdas", "Exaustão Brayton",
+                      "Exaustão SRA", "Exaustão"]
+            pos = [(0.10, 0.7), (0.65, 0.05), (0.65, 0.7), (6, 6), (0.5, 0.35), (6, 6), (0.85, 0.02),
+                   (0.8, 0.43), (0.81, 0.22), (1.0, 0.192), (0.94, 0.13), (0.95, 0.33)]
+            annotations = [f"<b>{v:.2f} kW</b>".replace(".", ",") for v in value]
+        elif lang.lower() == "en":
+            labels = ["Fuel", "Brayton", "ARS", "HDH", "Products", "Losses", "Brayton Outlet",
+                      "ARS Outlet", "Outlet"]
+            pos = [(0.12, 0.7), (0.65, 0.05), (0.65, 0.7), (6, 6), (0.5, 0.35), (6, 6), (0.85, 0.02),
+                   (0.8, 0.43), (0.81, 0.22), (1.0, 0.192), (0.94, 0.13), (0.95, 0.33)]
+            annotations = [f"<b>{v:.2f} kW</b>" for v in value]
+        else:
+            raise ValueError("Lang should be pt or en.")
+
+        node_colors = ["#141B41" for i in range(9)]
+        link_colors = ["rgba(48, 107, 172, 0.5)", "rgba(111, 156, 235, 0.5)", "rgba(152, 185, 242, 0.5)", "rgba(145, 142, 244, 0.5)",
+                       "rgba(255, 215, 0, 1)", "rgba(255,0,0, 1)", "rgba(111, 156, 235, 0.5)", "rgba(152, 185, 242, 0.5)", "rgba(145, 142, 244, 0.5)"]
+        return (labels, pos, source, target, value, node_colors, link_colors, annotations)
+
+    def generate(self, lang: str = "pt"):
+        labels, pos, source, target, value, node_colors, link_colors, annotations = self.prepare_componentes(lang)
+
+        labels = [f"<b>{label}</b>" for label in labels]
 
         fig = go.Figure(data=[go.Sankey(
             valueformat=".2f",
             valuesuffix=" kW",
             arrangement="snap",
             node=dict(
-                pad=15,
+                pad=5,
                 thickness=10,
                 line=dict(color="black", width=0.5),
                 label=labels,
@@ -102,19 +118,30 @@ class ModelSankey:
                 label=[f"{v:.2f} kW" for v in value]
             ))])
 
-        fig.update_layout(title_text=self.display_title, font_size=14, width=1000, height=500, showlegend=True)
-        final_path = os.path.join(self.paths["data"], "sankey_diagram.pdf")
+        for p, a in zip(pos, annotations):
+            fig.add_annotation(x=p[0], y=p[1],
+                               text=a,
+                               showarrow=False,
+                               font=dict(
+                size=28,
+            )
+            )
+
+        fig.update_layout(font_size=24, width=1550, height=650)
+        final_path = os.path.join(self.paths["data"], f"sankey_diagram_{lang}.pdf")
+        fig.write_image(final_path)
+        final_path = os.path.join(self.paths["data"], f"sankey_diagram_{lang}.jpg")
         fig.write_image(final_path)
         fig.show()
 
 
 def main():
-    EES_model = r'C:\Users\55199\Meu Drive\[Unicamp]\[Dissertação]\01 - Algoritmo\Analise\trigeracao_LiBrH2O.EES'
+    EES_model = r"C:\Root\Drive\Unicamp\[Unicamp]\[Dissertação]\01 - Algoritmo\Analise\trigeracao_LiBrH2O.EES"
     run_id = "CASO BASE"
 
-    sankey = ModelSankey(EES_model, run_id, "Caso Base")
-    sankey.load_exergy_data()
-    sankey.generate()
+    sankey = ModelSankey(EES_model, run_id)
+    sankey.generate(lang="en")
+    sankey.generate(lang="pt")
 
 
 if __name__ == "__main__":
